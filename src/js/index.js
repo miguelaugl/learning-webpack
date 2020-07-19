c5a292a4b568d2b1b04b6c323a323c419ebc0ff5
Miguel <miguelaugl@outlook.com>
2020-07-19 01:46:34 -0300

    feat: projeto finalizado

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../css/styles.scss';
+
+const textComponent = () => {
+  const elH1 = document.createElement('h1');
+  elH1.innerHTML = 'Olá, mundo';
+  elH1.classList.add('title');
+  return elH1;
+};
+
+document.body.appendChild(textComponent());
